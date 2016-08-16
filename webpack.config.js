06a7ede12b8db3cde325a66cfb1bc0849e1a872a
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2016-08-16 13:36:12 -0700

    Got the thing working with a few extensions. Todo: build in lots of other dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const vaidator = require('webpack-validator');
 
-module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+const common = {
+    devtool: 'source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
 };
+
+module.exports = vaidator(common);
