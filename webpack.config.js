4630d7b593d4c1a9c6d42be3312c82d59a48b1d1
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2016-08-16 17:12:17 -0700

    basic version working

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
 const vaidator = require('webpack-validator');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
 
-const common = {
+const ENV = {
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 8080
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const PATHS = {
+    dir: __dirname,
+    src: path.join(__dirname, 'src'),
+    js: path.join(__dirname, 'src', 'js'),
+    css: path.join(__dirname, 'src', 'css'),
+    build: path.join(__dirname, 'dist'),
+    cache: path.join(__dirname, 'cache')
+};
+
+const common_build = {
     devtool: 'source-map',
     entry: [
         'webpack-dev-server/client?http://localhost:3000',
@@ -10,10 +28,13 @@ const common = {
         './src/index'
     ],
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist/'
-    },
+        path: path.join(__dirname, PATHS.build),
+        filename: '[name].bundle.js',
+        publicPath: PATHS.build
+    }
+};
+
+const dev_build = {
     plugins: [
         new webpack.HotModuleReplacementPlugin()
     ],
@@ -21,9 +42,9 @@ const common = {
         loaders: [{
             test: /\.js$/,
             loaders: ['react-hot', 'babel'],
-            include: path.join(__dirname, 'src')
+            exclude: path.join(__dirname, 'node_modules')
         }]
     }
 };
 
-module.exports = vaidator(common);
+module.exports = vaidator(merge(common_build, dev_build));
